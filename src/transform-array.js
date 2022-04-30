1fa86ba43c7aa375eac672ef8b61c01c872da1b6
RichardPickman <drezzerock@gmail.com>
2022-04-30 19:38:57 +0200

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`)
+
+  const copyArr = [...arr];
+  const flags = ['--discard-next', '--discard-prev', '--double-next', '--double-prev', 'remove']
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      if (arr[i + 1] === 'remove') continue
+
+      copyArr[i + 1] = 'remove'
+    }
+    if (arr[i] === '--discard-prev') {
+      if (arr[i - 1] === 'remove') continue
+
+      copyArr[i - 1] = 'remove'
+    }
+    if (arr[i] === '--double-next') {
+      copyArr[i] = copyArr[i + 1]
+    }
+    if (arr[i] === '--double-prev') {
+      copyArr[i] = copyArr[i - 1]
+    }
+  }
+
+  copyArr.forEach(item => {
+    if (flags.includes(item) || item === undefined) return
+    result.push(item)
+  })
+
+  return result;
 }
 
 module.exports = {
