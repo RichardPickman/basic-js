36ec58823a7d9f0a025e0e3e5092759d1d1b76d2
RichardPickman <drezzerock@gmail.com>
2022-05-02 15:57:46 +0200

    feat: solve 90% of vigener cypher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(arg = true) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.reverse = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+    this.getAlphabet = arg ? this.alphabet : this.reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key, value) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!')
+
+    const messageArr = this.getIndexes(message);
+    const keysArr = this.getIndexesKey(message, key);
+    const encryptedArr = [];
+
+    for (let i = 0, j = 0; i < messageArr.length; i++) {
+      if (typeof messageArr[i] === 'string') {
+        encryptedArr.push(messageArr[i])
+      } else if (this.getAlphabet.includes(this.getAlphabet[messageArr[i]])) {
+        const enc = (messageArr[i] + keysArr[j]) % 26;
+        const dec = (messageArr[i] - keysArr[j]) < 0 ? ((messageArr[i] - keysArr[j]) + 26) % 26 : (messageArr[i] - keysArr[j]) % 26;
+  
+        encryptedArr.push(value === 'decrypt' ? this.getAlphabet[dec] : this.getAlphabet[enc])
+
+        j++
+      }
+    }
+    
+    return encryptedArr.join('');
+  }
+
+  decrypt(message, key) {
+    return this.encrypt(message, key, 'decrypt')
+  }
+
+  getIndexes(word) {
+    const arrayOfWord = [...word.toUpperCase()];
+    const messageArr = [];
+
+    arrayOfWord.forEach(letter => {
+      if (!this.getAlphabet.includes(letter)) {
+        messageArr.push(letter)
+      } else {
+        const item = this.getAlphabet.indexOf(letter)
+        messageArr.push(item);
+      }
+    })
+
+    return messageArr;
+  }
+
+  getIndexesKey(word, key) {
+    const keys = [];
+    let lastIndex = 0;
+    const keysArr = [...key]
+
+    while (keys.length !== word.length) {
+      if (lastIndex === keysArr.length) {
+        lastIndex = 0;
+        continue;
+      }
+      
+      keys.push(keysArr[lastIndex])
+
+      lastIndex++
+    }
+
+    return this.getIndexes(keys.join(''))
   }
 }
 
